d1de2ee59e06f08fbe95107075716de397d79b5d
teleivo <mrteleivo@gmail.com>
2015-02-13 18:28:24 +0000

    add server.xml template to define http port
    * add class parameter http_port with default value 8080
    * add server.xml.erb which is rendered with variable http_port
     and put into $CATALINA_HOME/conf/

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,30 @@
 class tomcat6 (
     $user = 'tomcat6',
-    $user_home = '/opt/tomcat6') {
-    
+    $user_home = '/opt/tomcat6',
+    $http_port = '8080') {
+
     class { 'tomcat6::install': user => $user,
                                 user_home => $user_home,
+                                http_port => $http_port,
     }
 }
 
 class tomcat6::install (
     $user = undef,
-    $user_home = undef) {
-    
+    $user_home = undef,
+    $http_port = undef) {
+
     $tomcat_url = 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.29/bin/apache-tomcat-6.0.29.tar.gz'
     $tomcat_archive_name = 'apache-tomcat-6.0.29.tar.gz'
     $tomcat_folder_name = 'apache-tomcat-6.0.29'
     $tomcat_dest_path = "${user_home}/${tomcat_folder_name}"
     $tomcat_dest_symlink = "${user_home}/tomcat6"
+    $tomcat_http_port = "${http_port}"
 
     user { $user:
         ensure  => present,
-	home => "${user_home}",
-	managehome => true,
+        home => "${user_home}",
+        managehome => true,
     }->
 
     exec { 'tomcat6-wget':
@@ -37,6 +41,12 @@ class tomcat6::install (
         group   => "${user}",
         path    => '/bin',
         command => "tar xzf ${tomcat_archive_name}",
+    }->
+
+    file { 'server.xml':
+        path    => "${tomcat_dest_path}/conf/server.xml",
+        ensure  => file,
+        content => template("tomcat6/server.xml.erb"),
     }->
 
     file { 'tomcat6-symlink':
