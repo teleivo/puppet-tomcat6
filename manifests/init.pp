82789002ef478591d0f20aedccf7a08d15b6c9ac
teleivo <mrteleivo@gmail.com>
2015-02-13 17:56:17 +0000

    execute commands as tomcat user and pass home dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,51 +1,49 @@
 class tomcat6 (
-    $user = undef) {
+    $user = undef,
+    $user_home = undef) {
     
-    class { 'tomcat6::install': user => $user }
+    class { 'tomcat6::install': user => $user,
+                                user_home => $user_home,
+    }
 }
 
 class tomcat6::install (
-    $user = undef) {
+    $user = undef,
+    $user_home = undef) {
     
     $tomcat_url = 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.29/bin/apache-tomcat-6.0.29.tar.gz'
     $tomcat_archive_name = 'apache-tomcat-6.0.29.tar.gz'
     $tomcat_folder_name = 'apache-tomcat-6.0.29'
-    $install_base_dir = '/opt'
-    $install_path = "${install_base_dir}/${tomcat_folder_name}"
+    $tomcat_dest_path = "${user_home}/${tomcat_folder_name}"
+    $tomcat_dest_symlink = "${user_home}/tomcat6"
 
     user { $user:
-        ensure  => present
+        ensure  => present,
+	home => "${user_home}",
+	managehome => true,
     }->
 
     exec { 'tomcat6-wget':
-        cwd     => '/tmp',
-        user    => 'root',
-        group   => 'root',
+        cwd     => "${user_home}",
+        user    => "${user}",
+        group   => "${user}",
         path    => '/usr/bin',
-        command => "wget ${tomcat_url}"
+        command => "wget ${tomcat_url}",
     }->
 
     exec { 'tomcat6-untar':
-        cwd     => '/tmp',
-        user    => 'root',
-        group   => 'root',
+        cwd     => "${user_home}",
+        user    => "${user}",
+        group   => "${user}",
         path    => '/bin',
-        command => "tar xzf ${tomcat_archive_name} -C ${install_base_dir}"
-    }->
-
-    exec { 'tomcat6-chown':
-        cwd     => "${install_base_dir}",
-        user    => 'root',
-        group   => 'root',
-        path    => '/bin',
-        command => "chown -R ${user}.${user} ${install_path}"
+        command => "tar xzf ${tomcat_archive_name}",
     }->
 
     file { 'tomcat6-symlink':
-        path    => "${install_base_dir}/tomcat6",
         ensure  => link,
-        target  => "${install_path}",
-        owner   => "${user}",
-        group   => "${user}",
+        owner   => "$user",
+        group   => "$user",
+        path    => "${tomcat_dest_symlink}",
+        target  => "${tomcat_dest_path}",
     }
 }
