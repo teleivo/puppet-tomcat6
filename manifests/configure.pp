86abaf7767bc817239e953d265ebf56c10586733
teleivo <mrteleivo@gmail.com>
2015-02-25 12:38:28 +0000

    use file resource title to set attribute path
    to reduce potential conflict with other module file resource titles

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -4,14 +4,15 @@ class tomcat6::configure (
     $tomcat6_conf_users = undef,
 ) {
 
-    file { 'server.xml':
-        path    => "${tomcat6_home_path}/conf/server.xml",
+    $tomcat6_server_xml_path = "${tomcat6_home_path}/conf/server.xml"
+    $tomcat6_users_xml_path = "${tomcat6_home_path}/conf/tomcat-users.xml"
+
+    file { "${tomcat6_server_xml_path}":
         ensure  => file,
         content => template("tomcat6/server.xml.erb"),
     }->
 
-    file { 'tomcat-users.xml':
-        path    => "${tomcat6_home_path}/conf/tomcat-users.xml",
+    file { "${tomcat6_users_xml_path}":
         ensure  => file,
         content => template("tomcat6/tomcat-users.xml.erb"),
     }
